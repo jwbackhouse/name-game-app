8c2ca8a0e16fabff703c0c307bb314a5e0665a0f
James Backhouse <jwbackhouse@gmail.com>
2020-06-11 19:47:13 +0100

    Removed measurementId from firebase config.

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -7,8 +7,7 @@ const firebaseConfig = {
   projectId: process.env.FIREBASE_PROJECT_ID,
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID
+  appId: process.env.FIREBASE_APP_ID
 };
 
 firebase.initializeApp(firebaseConfig);
