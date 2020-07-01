c735a206c2f23a5ae9f2e589cf80575434a354d6
James Backhouse <jwbackhouse@gmail.com>
2020-07-01 10:48:28 +0100

    Removed state.user from store config.

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -2,7 +2,6 @@ import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk'; // allows asynchronous calls to Firebase from actions
 import authReducer from '../reducers/auth';
 import namesReducer from '../reducers/names';
-import userReducer from '../reducers/user';
 import playersReducer from '../reducers/players';
 import gameReducer from '../reducers/game';
 
@@ -14,7 +13,6 @@ export default () => {
     combineReducers({   // Takes object as an argument - with root state name as key, and reducer as the value
       auth: authReducer,
       names: namesReducer,
-      user: userReducer,
       players: playersReducer,
       game: gameReducer
     }),
