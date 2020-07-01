16a2392645a9d3d96c5ffa985ffa46f8e18bd4e2
James Backhouse <jwbackhouse@gmail.com>
2020-07-01 21:02:44 +0100

    Refactor code (slightly).

--- src/tests/components/RegisterPage.test.js
+++ src/tests/components/RegisterPage.test.js
@@ -5,75 +5,79 @@ import players from '../fixtures/testPlayers';
 import { RegisterPage } from '../../components/RegisterPage';
 
 
-let wrapper, startAddGameInfo, fetchData, endFetchData, updateDisplayName, history, newPlayer, defaultState;
-beforeEach(() => {
-  startAddGameInfo = jest.fn();
-  fetchData = jest.fn();
-  endFetchData = jest.fn();
-  updateDisplayName= jest.fn();
-  history={ push: jest.fn() }
-  wrapper = shallow(
-    <RegisterPage
-      // Redux state
-      auth={ auth }
-      players={ players }
-      // Redux dispatch
-      startAddGameInfo={ startAddGameInfo }
-      fetchData={ fetchData }
-      endFetchData={ endFetchData }
-      updateDisplayName={ updateDisplayName }
-      history={ history }
-    />
-  );
-  newPlayer = {
+describe('<RegisterPage />', () => {
+  const startAddGameInfo = jest.fn();
+  const fetchData = jest.fn();
+  const endFetchData = jest.fn();
+  const updateDisplayName= jest.fn();
+  const history={ push: jest.fn() };
+  const newPlayer = {
     username: 'Bob',
     team: 'B',
   };
-  defaultState = {
+  const defaultState = {
     username: '',
     team: 'A',
     error: ''
   };
-});
-
-test('Should render RegisterPage correctly', () => {
-  expect(wrapper).toMatchSnapshot();
-});
-
-// User name field
-test('Should update state.username on input change', () => {
-  const value = 'Joe Bloggs';
-  wrapper.find('input').simulate('change', {
-    target: {value}    // pass second argument for 'e.target.value'
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(
+      <RegisterPage
+        // Redux state
+        auth={ auth }
+        players={ players }
+        // Redux dispatch
+        startAddGameInfo={ startAddGameInfo }
+        fetchData={ fetchData }
+        endFetchData={ endFetchData }
+        updateDisplayName={ updateDisplayName }
+        history={ history }
+      />
+    );
   });
-  expect(wrapper.state('username')).toBe(value);   // this syntax is enzyme-specific
-});
-
-// Team selector
-test('Should update state.username on input change', () => {
-  const value = 'B';
-  wrapper.find('select').simulate('change', {
-    target: {value}    // pass second argument for 'e.target.value'
+  
+  test('Should render RegisterPage correctly', () => {
+    expect(wrapper).toMatchSnapshot();
   });
-  expect(wrapper.state('team')).toBe(value);   // this syntax is enzyme-specific
-});
-
-// On submit
-test('Should render error if no name entered', () => {
-  wrapper.setState({ ...blankAuth })
-  wrapper.find('form').simulate('submit', {
-    preventDefault: () => {}
+  
+  // User name field
+  test('Should update state.username on input change', () => {
+    const value = 'Joe Bloggs';
+    wrapper.find('input').simulate('change', {
+      target: {value}    // pass second argument for 'e.target.value'
+    });
+    expect(wrapper.state('username')).toBe(value);   // this syntax is enzyme-specific
   });
-  console.log(wrapper.state('username'));
-  expect(wrapper.state('error')).toBe('^^Please enter your name');
-});
-
-test('Should submit correctly when name & team entered', () => {
-  wrapper.setState({ ...newPlayer });
-  wrapper.find('form').simulate('submit', {
-    preventDefault: () => {}
+  
+  // Team selector
+  test('Should update state.team on input change', () => {
+    const value = 'B';
+    wrapper.find('select').simulate('change', {
+      target: {value}    // pass second argument for 'e.target.value'
+    });
+    expect(wrapper.state('team')).toBe(value);   // this syntax is enzyme-specific
   });
-  expect(startAddGameInfo).lastCalledWith(newPlayer);
-  expect(updateDisplayName).lastCalledWith(newPlayer.username);
-  expect(history.push).lastCalledWith('/setup');
+  
+  // On submit
+  describe('onSubmit', () => {
+    test('Should render error if no name entered', () => {
+      wrapper.setState({ ...blankAuth })
+      wrapper.find('form').simulate('submit', {
+        preventDefault: () => {}
+      });
+      console.log(wrapper.state('username'));
+      expect(wrapper.state('error')).toBe('^^Please enter your name');
+    });
+    
+    test('Should call startAddGameInfo() & updateDisplayName(), and redirect to /setup', () => {
+      wrapper.setState({ ...newPlayer });
+      wrapper.find('form').simulate('submit', {
+        preventDefault: () => {}
+      });
+      expect(startAddGameInfo).lastCalledWith(newPlayer);
+      expect(updateDisplayName).lastCalledWith(newPlayer.username);
+      expect(history.push).lastCalledWith('/setup');
+    });
+  });
 });
