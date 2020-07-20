ae6b81da0b836ca629d32405d849e301d85f4b40
James Backhouse <jwbackhouse@gmail.com>
2020-07-20 21:08:25 +0100

    Update Countdown tests.

---
+++ src/components/Countdown.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import moment from 'moment';
+// import { timerLength } from '../app';
+import database from '../firebase/firebase';
+
+// TODO - link state.timerLength to admin page
+export class Countdown extends React.Component {
+  state = {
+    timerOn: false,
+    timerTime: 0,
+    timerStart: 0,
+    timerLength: 120000,
+    message: '',
+  }
+  
+  componentDidMount = () => {
+    // Check for start time on Firebase
+    database.ref('game/startTime').on('value', snapshot => {
+      const startTime = snapshot.val();
+      if (startTime) {
+        this.setState({ timerStart: startTime, message: '' });
+        this.startTimer();
+      } else {
+        this.setState({ message: 'Hang on a second...' });
+      }
+    });
+  }
+  
+  componentWillUnmount = () => {
+    database.ref('game/startTime').off();
+    this.stopTimer();
+  }
+  
+  startTimer = () => {
+    const { timerTime, timerLength, timerStart } = this.state;
+    
+    this.setState({
+      timerOn: true,
+      timerTime: timerTime
+    });
+    
+    this.timer = setInterval(() => {
+      const timeLeft = timerLength - timerTime;
+      if (timeLeft >= 0)  {
+        this.setState({ timerTime: Date.now() - timerStart });
+      } else {
+        clearInterval(this.timer);
+        this.setState({ timerOn: false });
+        this.props.onFinished();
+      }
+    }, 50);
+  }
+
+  stopTimer = () => {
+    clearInterval(this.timer);
+    this.setState({ timerOn: false });
+  };
+  
+  // // Unused functionality
+  // resetTimer = () => {
+  //   this.setState({
+  //     timerTime: 0,
+  //     timerStart: 0
+  //   });
+  // };
+  
+  
+  render = () => {
+    const { timerLength, timerTime, message } = this.state;
+    const timeLeft = Math.max(0, timerLength - timerTime);
+    const messageElement = message && message;
+    return (
+      <div>
+        <p>{ messageElement }</p>
+        <h3>Time left: {moment(timeLeft).format('m:ss')}</h3>
+      </div>
+    )
+  }
+};
+
+export default Countdown;
---
+++ src/tests/components/Countdown.test.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import database from '../../firebase/firebase';
+import { Countdown } from '../../components/Countdown';
+
+// // This and commented-out lines below can be used to mock Firebase
+// jest.mock('../../firebase/firebase', () => {
+//   const on = jest.fn();
+//   return {
+//     ref: jest.fn(() => ({
+//       on,
+//     })),
+//   };
+// });
+
+describe('<Countdown />', () => {
+  let wrapper, on;
+  const onFinished = jest.fn();
+  beforeEach(() => {
+    // on = database.ref().on;
+    wrapper = shallow(<Countdown onFinished={ onFinished }/>);
+  })
+  
+  test('Should render Countdown with time remaining', (done) => {
+    const timeNow = Date.now();
+    database.ref('game/startTime').set(timeNow).then(() => done());    // Call done() once database promise has resolved
+    expect(wrapper).toMatchSnapshot();
+    // expect(on).toBeCalled();
+  });
+  
+    
+  test('Should render Countdown with please wait message', (done) => {
+    const timeNow = new Date(2020, 0, 1);
+    database.ref('game/startTime').set('').then(() => done());
+    expect(wrapper).toMatchSnapshot();
+  });
+  
+  test('Should call onFinished when timer expires', () => {
+    wrapper.setState({ timerLength: 0 });
+    setTimeout(() => {
+      expect(wrapper).toMatchSnapshot()
+      expect(onFinished).toBeCalled();
+    }, 1000);
+  });
+});
