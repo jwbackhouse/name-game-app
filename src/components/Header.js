fe2da1ed3f28aa8ac3cc36f0d8d6e2dbce58646c
James Backhouse <jwbackhouse@gmail.com>
2020-07-20 23:05:29 +0100

    Add link to admin page in header.

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,16 +4,23 @@ import { connect } from 'react-redux';
 import { startLogout } from '../actions/auth';
 
 
-export const Header = ({startLogout}) => (
-  <div className='header'>
-    <div className='content-container'>
-      <div className='header__content'>
-        <h1 className='header__title'>The Name Game</h1>
-        <button onClick={ startLogout }>Sign out</button>
+export const Header = ({ startLogout, history }) => {
+  const onClick = () => {
+    history.push('/admin');
+  };
+  
+  return (
+    <div className='header'>
+      <div className='content-container'>
+        <div className='header__content'>
+          <h1 className='header__title'>The Name Game</h1>
+          <button onClick={ startLogout }>Sign out</button>
+          <button onClick = { onClick }>Settings</button>
+        </div>
       </div>
     </div>
-  </div>
-);
+  )
+};
 
 const mapDispatchToProps = (dispatch) => ({
   startLogout: () => dispatch(startLogout())
