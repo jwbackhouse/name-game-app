a749c859fbfce81271a437b40dbde4db9fb376de
James Backhouse <jwbackhouse@gmail.com>
2020-08-31 12:15:01 +0100

    Fix names selector function.

--- src/selectors/selectUsersNames.js
+++ src/selectors/selectUsersNames.js
@@ -1,5 +1,5 @@
 // Choose name and return starting team letter
 export default ((names, uid) => {
-  const usersNames = names.filter(name => name.uid === uid);
+  const usersNames = names.filter(name => name.playerUid === uid);
   return usersNames;
 });
