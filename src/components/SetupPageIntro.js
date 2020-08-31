00466b15696734b052191956d87ee5c13a24273b
James Backhouse <jwbackhouse@gmail.com>
2020-08-31 10:47:41 +0100

    Improve user messaging.

--- src/components/SetupPageIntro.js
+++ src/components/SetupPageIntro.js
@@ -5,7 +5,10 @@ export const SetupPageIntro = ({ remainingNames, word }) => (
   <div>
     <h1>Choose your names</h1>
     <p className='tip'>Friendly tip: It's a good idea to pick names everyone has heard of</p>
-    <p className=''>{ `You have ${ remainingNames } ${ word } to add` }</p>
+    { remainingNames !== 0
+        ? <p className=''>{ `You have ${ remainingNames } ${ word } to add` }</p>
+        : <p>You're good to go</p>
+    }
   </div>
 );
 
