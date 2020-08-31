0597177f6b2518530803c80f9dda6c96383c2221
James Backhouse <jwbackhouse@gmail.com>
2020-08-31 07:24:01 +0100

    Refine button styling.

--- src/components/PassedNamesButton.js
+++ src/components/PassedNamesButton.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const PassedNamesButton = ({ toggleViewPassedNames, viewPassedNames }) =>
-  <button onClick={ toggleViewPassedNames }>
+  <button className='button button--delete' onClick={ toggleViewPassedNames }>
     { viewPassedNames ? 'Return to unguessed names' : 'Re-try your passed names' }
   </button>;
 
--- src/components/SetupPageIntro.js
+++ src/components/SetupPageIntro.js
@@ -2,7 +2,7 @@ import React from 'react';
 import TeamList from './TeamList';
 
 export const SetupPageIntro = ({ remainingNames, word }) => (
-  <div className='content-container'>
+  <div>
     <h1>Choose your names</h1>
     <p className='tip'>Friendly tip: It's a good idea to pick names everyone has heard of</p>
     <p className=''>{ `You have ${ remainingNames } ${ word } to add` }</p>
