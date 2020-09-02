3a4c8b7a41127c91efb1a1e1e7e9f020919d6093
James Backhouse <jwbackhouse@gmail.com>
2020-09-02 20:27:25 +0100

    Improve EndPage score styling.

--- src/components/EndPage.js
+++ src/components/EndPage.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 export const EndPage = (props) => {
   return (
-    <div>
+    <div className='content-container'>
       <h1>{ props.message }</h1>
-      <p>Team A scored { props.teamAScore }</p>
-      <p>Team B scored { props.teamBScore }</p>
+      <p>Team A scored ... <span className='score'>{ props.teamAScore }</span></p>
+      <p>Team B scored ... <span className='score'>{ props.teamBScore }</span></p>
       <button className='button' onClick={ props.onClick }>Play again</button>
     </div>
   );
