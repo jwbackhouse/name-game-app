c1486598709245d97408cdead9094a95daa8d627
James Backhouse <jwbackhouse@gmail.com>
2020-09-03 10:42:59 +0100

    Remove unused component.

--- src/components/PassedNamesButton.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const PassedNamesButton = ({ toggleViewPassedNames, viewPassedNames }) =>
-  <button className='button button--text-link' onClick={ toggleViewPassedNames }>
-    { viewPassedNames ? 'Return to unguessed names' : 'Re-try your passed names' }
-  </button>;
-
-export default PassedNamesButton;
